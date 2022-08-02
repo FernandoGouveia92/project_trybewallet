92b66d90bdb84a9bb317b364ea9fff9a2935d67f
Fernando Gouveia <fernando_gouveia2@hotmail.com>
2022-08-02 15:12:36 -0300

    7 feito

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { expensesToFillTable } = this.props;
     return (
-      <div>
+      <table>
         Table
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-      </div>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expensesToFillTable
+              .map((i) => (
+                <tr key={ i.id }>
+                  <td>{i.description}</td>
+                  <td>{i.tag}</td>
+                  <td>{i.method}</td>
+                  <td>{Number(i.value).toFixed(2)}</td>
+                  <td>{i.exchangeRates[i.currency].name}</td>
+                  <td>{Number(i.exchangeRates[i.currency].ask).toFixed(2)}</td>
+                  <td>{Number(i.value * i.exchangeRates[i.currency].ask).toFixed(2)}</td>
+                  <td>Real</td>
+                </tr>
+              ))
+          }
+        </tbody>
+      </table>
     );
   }
 }
+
+Table.propTypes = {
+  expensesToFillTable: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expensesToFillTable: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps, null)(Table);
